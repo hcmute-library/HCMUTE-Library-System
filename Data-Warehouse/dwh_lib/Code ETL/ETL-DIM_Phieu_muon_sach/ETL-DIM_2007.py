0bc9abd7d35c1cea5b4084d093ef0bd32fe0756f
Phung Cao Thi Ngoc <phungcao.1811@gmail.com>
2024-11-29 14:48:10 +0700

    updte

--- Data-Warehouse/dwh_lib/Code ETL/ETL-DIM_Phieu_muon_sach/ETL-DIM_2007.py
+++ Data-Warehouse/dwh_lib/Code ETL/ETL-DIM_Phieu_muon_sach/ETL-DIM_2007.py
@@ -56,7 +56,7 @@ def fetch_data_and_process():
                                 So_luot_gia_han,
                                 Note
                             FROM An_pham_cho_muon
-                            WHERE YEAR(Ngay_muon) = 2004
+                            WHERE YEAR(Ngay_muon) = 2007
                             """
     df_apcm = fetch_data_in_batches(query_Anphamchomuon, conn_libol, batch_size=100) # Gọi hàm để lấy dữ liệu
 
@@ -70,7 +70,7 @@ def fetch_data_and_process():
                                 So_ngay_qua_han,
                                 Tien_phat
                             FROM Lich_su_muon_sach
-                            WHERE YEAR(Ngay_muon) = 2004
+                            WHERE YEAR(Ngay_muon) = 2007
                             """
     df_lscm = fetch_data_in_batches(query_Lichsumuonsach, conn_libol, batch_size=100) # Gọi hàm để lấy dữ liệu
 
@@ -189,11 +189,11 @@ default_args = {
 }
 
 with DAG(
-    'etl_dim_pms_2004',
+    'etl_dim_pms_2007',
     default_args=default_args,
     description='etl dữ liệu phiếu mượn sách',
     schedule_interval='@once',
-    start_date=datetime(2024, 11, 29, 5, 0),
+    start_date=datetime(2024, 11, 29, 22, 0),
     catchup=False,
     tags=['etl'],
 ) as dag:
