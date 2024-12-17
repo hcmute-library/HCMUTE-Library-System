58f2334c3fcf867ff87e3693f0e444b4a2c216bc
Phung Cao Thi Ngoc <69232119+NgocPhungggg@users.noreply.github.com>
2024-12-18 03:23:31 +0700

    Update ETL-FACT_Thong_ke_thu_vien_2020.py

--- Data-Warehouse/dwh_library/Code-ETL/ETL-FACT_Thu_vien/ETL-FACT_Thong_ke_thu_vien_2020.py
+++ Data-Warehouse/dwh_library/Code-ETL/ETL-FACT_Thu_vien/ETL-FACT_Thong_ke_thu_vien_2020.py
@@ -65,7 +65,7 @@ with DAG(
     default_args=default_args,
     description='Load data vào bảng Thống Kê Thư Viện',
     schedule_interval='@once',
-    start_date=datetime(2024, 12, 1, 4, 30),
+    start_date=datetime(2024, 12, 18, 4, 30),
     catchup=False,
     tags=['etl'],
 ) as dag:
