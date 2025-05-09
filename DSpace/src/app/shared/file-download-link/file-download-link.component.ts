0ead4ca5d6017ecde817128275496e09b8b08723
NgocPhunggggg <phungcao.1811@gmail.com>
2025-05-09 21:19:14 +0700

    update front end dspace

--- DSpace/src/app/shared/file-download-link/file-download-link.component.ts
+++ DSpace/src/app/shared/file-download-link/file-download-link.component.ts
@@ -24,19 +24,7 @@ export class FileDownloadLinkComponent implements OnInit {
     if (!this.bitstreamHref) return;
 
     const url = this.bitstreamHref + '#toolbar=0&navpanes=0&scrollbar=0';
-
-    const newWindow = window.open('', '_blank');
-    if (newWindow) {
-      newWindow.document.write(`
-        <html>
-          <head><title>Xem PDF</title></head>
-          <body style="margin:0">
-            <iframe src="${url}" style="width:100vw; height:100vh; border:none;"></iframe>
-          </body>
-        </html>
-      `);
-      newWindow.document.close();
-    }
+    window.open(url, '_blank');
   }
 }
 
